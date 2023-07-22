f1ce94847d6ac8e63fb4d0ccd09da2fd920456b9
Lea MM <leatex_99@yahoo.com>
2023-07-22 12:47:17 -0300

    project rename

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/UNQ-UI-LM-trabajo-final-piedra-papel-tijera-lagarto-spock/"
+  base: "/UNQ-UI-2023c1-TFI-piedra-papel-tijera-lagarto-spock/"
 })
